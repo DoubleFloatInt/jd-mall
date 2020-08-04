e1cfa87e513d4b76afcf11562177041c04d542ba
Double327 <180303665@qq.com>
2020-08-04 10:10:34 +0800

    测试Demo

---
+++ src/components/DemoTest.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="first">
+		我们组最牛逼!!!
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "DemoTest"
+    }
+</script>
+
+<style scoped>
+	.first {
+		font-size: 66px;
+	}
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,13 @@
+import HelloWorld from "../components/HelloWorld";
+import DemoTest from "../components/DemoTest";
+
+export default [
+    {
+        path: '/',
+        component: HelloWorld
+    },
+    {
+        path: '/hello',
+        component: DemoTest
+    }
+]
