0d15bc00fe25289612447aec7c05b6698cd812c2
Double327 <180303665@qq.com>
2020-08-10 11:09:49 +0800

    搜索结果页面跳转

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,119 +1,124 @@
 <template>
-	<div>
-		<header id="top">
-			<div id="top_box">
-				<img class="lf" src="../../assets/images/star.jpg">
-				<a class="lf" href="#">收藏京东</a>    <!-- lf左浮动 -->
-				<ul class="rt">        <!-- rt右浮动 -->
-					<li class="login-register-area">
-						玛卡巴卡，你好！欢迎来到京东
-						<router-link to="/login" tag="a">你好，请登录</router-link>
-						<router-link to="/register" tag="a">[免费注册]</router-link>
-					</li>
-					<li>
-						<b></b><!--边框-->
-						<router-link to="/orderList" tag="a">我的订单</router-link>
-					</li>
-					<li class="vip">
-						<b></b>
-						<a href="#">会员俱乐部</a>
-					</li>
-					<li class="qiye">
-						<b></b>
-						<a href="#">企业频道</a>
-					</li>
-					<li class="app_jd">
-						<b></b>
-						<a href="#">手机京东</a>
-					</li>
-					<li class="service">
-						<b></b>
-						<a href="#">客户服务</a>
-						<!-- 弹出菜单 -->
-						<ul>
-							<li>
-								<a href="#">帮助中心</a>
-							</li>
-							<li>
-								<a href="#">售后服务</a>
-							</li>
-							<li>
-								<a href="#">在线客服</a>
-							</li>
-							<li>
-								<a href="#">投诉中心</a>
-							</li>
-							<li>
-								<a href="#">客服邮箱</a>
-							</li>
-						</ul>
-					</li>
-					<li class="">
-						<b></b>
-						<a href="#">网站导航</a>
-					</li>
-				</ul>
-			</div>
-		</header>
-		<!--2.logo和搜索框-->
-		<div id="top_main">
-			<!-- 2.1logo图片 -->
-			<router-link class="lf" tag="a" to="/">
-				<img alt="JD" src="../../assets/images/logo-201305.png"/>
-			</router-link>
-			<!-- 2.2搜索框和热门搜索 -->
-			<div id="search_box">
-				<!-- 2.2.1搜索框和按钮 -->
-				<div class="search">
-					<input class="txtsearch">
-					<button>搜索</button>
-				</div>
-				<!-- 2.2.2热门搜索 -->
-				<div class="hot_words">
-					<span>热门搜索:</span>
-					<a href="#">家纺11月大促</a>
-					<a href="#">彩虹电热毯</a>
-					<a href="#">阿迪达斯</a>
-					<a href="#">中国李宁</a>
-					<a href="#">VLONE</a>
-				</div>
-			</div>
-			<!-- 2.3我的京东 -->
-			<div id="my_jd">
-				我的京东
-				<b></b>
-				<!-- <a href="#" class="lf">
-					<img src="images/jd.jpg">
-				</a>
-				<ul>
-					<li>
-						<a href ="#">待发货</a>
-					</li>
-					<li>
-						<a href ="#">待收货</a>
-					</li>
-					<li>
-						<a href ="#">待评价</a>
-					</li>
-					<li>
-						<a href ="#">退换货</a>
-					</li>
-				</ul> -->
-			</div>
-			<!-- 2.4去购物车结算 -->
-			<div id="shopcar">
-				<b></b>
-				购物车结算
-			</div>
-			<!--3.页面主导航栏-->
-		</div>
-	</div>
+  <div>
+    <header id="top">
+      <div id="top_box">
+        <img class="lf" src="../../assets/images/star.jpg">
+        <a class="lf" href="#">收藏京东</a>    <!-- lf左浮动 -->
+        <ul class="rt">        <!-- rt右浮动 -->
+          <li class="login-register-area">
+            玛卡巴卡，你好！欢迎来到京东
+            <router-link to="/login" tag="a">你好，请登录</router-link>
+            <router-link to="/register" tag="a">[免费注册]</router-link>
+          </li>
+          <li>
+            <b></b><!--边框-->
+            <router-link to="/orderList" tag="a">我的订单</router-link>
+          </li>
+          <li class="vip">
+            <b></b>
+            <a href="#">会员俱乐部</a>
+          </li>
+          <li class="qiye">
+            <b></b>
+            <a href="#">企业频道</a>
+          </li>
+          <li class="app_jd">
+            <b></b>
+            <a href="#">手机京东</a>
+          </li>
+          <li class="service">
+            <b></b>
+            <a href="#">客户服务</a>
+            <!-- 弹出菜单 -->
+            <ul>
+              <li>
+                <a href="#">帮助中心</a>
+              </li>
+              <li>
+                <a href="#">售后服务</a>
+              </li>
+              <li>
+                <a href="#">在线客服</a>
+              </li>
+              <li>
+                <a href="#">投诉中心</a>
+              </li>
+              <li>
+                <a href="#">客服邮箱</a>
+              </li>
+            </ul>
+          </li>
+          <li class="">
+            <b></b>
+            <a href="#">网站导航</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <!--2.logo和搜索框-->
+    <div id="top_main">
+      <!-- 2.1logo图片 -->
+      <router-link class="lf" tag="a" to="/">
+        <img alt="JD" src="../../assets/images/logo-201305.png"/>
+      </router-link>
+      <!-- 2.2搜索框和热门搜索 -->
+      <div id="search_box">
+        <!-- 2.2.1搜索框和按钮 -->
+        <div class="search">
+          <input class="txtsearch">
+          <button @click="handleSearch">搜索</button>
+        </div>
+        <!-- 2.2.2热门搜索 -->
+        <div class="hot_words">
+          <span>热门搜索:</span>
+          <a href="#">家纺11月大促</a>
+          <a href="#">彩虹电热毯</a>
+          <a href="#">阿迪达斯</a>
+          <a href="#">中国李宁</a>
+          <a href="#">VLONE</a>
+        </div>
+      </div>
+      <!-- 2.3我的京东 -->
+      <div id="my_jd">
+        我的京东
+        <b></b>
+        <!-- <a href="#" class="lf">
+          <img src="images/jd.jpg">
+        </a>
+        <ul>
+          <li>
+            <a href ="#">待发货</a>
+          </li>
+          <li>
+            <a href ="#">待收货</a>
+          </li>
+          <li>
+            <a href ="#">待评价</a>
+          </li>
+          <li>
+            <a href ="#">退换货</a>
+          </li>
+        </ul> -->
+      </div>
+      <!-- 2.4去购物车结算 -->
+      <div id="shopcar">
+        <b></b>
+        购物车结算
+      </div>
+      <!--3.页面主导航栏-->
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Header"
+export default {
+  name: "Header",
+  methods: {
+    handleSearch() {
+      this.$router.push({path: '/search'})
     }
+  }
+}
 </script>
 
 <style scoped>
