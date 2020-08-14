cb5159951cdce7ac07838f1a501e883a65cfed15
Double327 <180303665@qq.com>
2020-08-14 22:13:58 +0800

    秒杀、特价页面

--- src/view/TeJia/index.vue
+++ src/view/TeJia/index.vue
@@ -1,26 +1,32 @@
 <template>
-	<div class="clearfix">
-		<!--3  特价-->
-		<div id="nav">
-			<div class="miaosha_top">
-				<ul class="lf">
-					<li><router-link to="/miaosha" tag="a">京东秒杀</router-link></li>
-					<li class="two"><a href="#">每日特价</a></li>
-					<li><router-link to="/shangou" tag="a">大牌闪购</router-link></li>
-					<li><router-link to="/pinlei" tag="a">品类秒杀</router-link></li>
-				</ul>
-			</div>
-		</div>
+  <div class="clearfix">
+    <!--3  特价-->
+    <div id="nav">
+      <div class="miaosha_top">
+        <ul class="lf">
+          <li>
+            <router-link to="/miaosha" tag="a">京东秒杀</router-link>
+          </li>
+          <li class="two"><a href="#">每日特价</a></li>
+          <li>
+            <router-link to="/shangou" tag="a">大牌闪购</router-link>
+          </li>
+          <li>
+            <router-link to="/pinlei" tag="a">品类秒杀</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
 
 
-		<!--顶部图片-->
-		<div id="photo">
-			<img src="../../assets/images/mtp/01.png">
-			<div class="fengding">
-				<a href="#" class="ftitle"></a>
-				<div class="last">
-					<div class="lasttitle">最后疯抢</div>
-					<div class="ltitle">
+    <!--顶部图片-->
+    <div id="photo">
+      <img src="../../assets/images/mtp/01.png">
+      <div class="fengding">
+        <a href="#" class="ftitle"></a>
+        <div class="last">
+          <div class="lasttitle">最后疯抢</div>
+          <div class="ltitle">
 				<span>
 					<i>1</i>
 					<i>3</i>
@@ -31,526 +37,183 @@
 					<i>5</i>
 					<i>2</i>
 				</span>
-					</div>
-				</div>
+          </div>
+        </div>
 
 
-				<div class="fpicture">
-					<div class="picture">
-						<a href="#" class="pic_a">
-							<div class="pic_f">
-								<img src="../../assets/images/mtp/001.jpg">
-							</div>
-							<div class="pictitle">酸枣汁果汁饮料</div>
-							<div class="picmoney">
-								<div class="pm_two">
+        <div class="fpicture">
+          <div class="picture">
+            <a href="#" class="pic_a">
+              <div class="pic_f">
+                <img src="../../assets/images/mtp/001.jpg">
+              </div>
+              <div class="pictitle">酸枣汁果汁饮料</div>
+              <div class="picmoney">
+                <div class="pm_two">
 						<span>
 							<em>￥</em>
 							9.9
 						</span>
-									<del>
-										￥
-										14.9
-									</del>
-								</div>
-							</div>
-							<div class="ysmoney">
-								<div>已售<span>134</span>件</div>
-							</div>
-						</a>
-					</div>
+                  <del>
+                    ￥
+                    14.9
+                  </del>
+                </div>
+              </div>
+              <div class="ysmoney">
+                <div>已售<span>134</span>件</div>
+              </div>
+            </a>
+          </div>
 
-					<div class="picture">
-						<a class="pic_a">
-							<div class="pic_f">
-								<img src="../../assets/images/mtp/001.jpg">
-							</div>
-							<div class="pictitle">天然芦荟胶</div>
-							<div class="picmoney">
-								<div class="pm_two">
+          <div class="picture">
+            <a class="pic_a">
+              <div class="pic_f">
+                <img src="../../assets/images/mtp/001.jpg">
+              </div>
+              <div class="pictitle">天然芦荟胶</div>
+              <div class="picmoney">
+                <div class="pm_two">
 					<span>
 						<em>￥</em>
 						9.9
 					</span>
-									<del>
-										￥
-										29.9
-									</del>
-								</div>
-							</div>
-							<div class="ysmoney">
-								<div>已售<span>134</span>件</div>
-							</div>
-						</a>
-					</div>
-					<div class="picture">
-						<a class="pic_a">
-							<div class="pic_f">
-								<img src="../../assets/images/mtp/001.jpg">
-							</div>
-							<div class="pictitle">天然苏打水</div>
-							<div class="picmoney">
-								<div class="pm_two">
+                  <del>
+                    ￥
+                    29.9
+                  </del>
+                </div>
+              </div>
+              <div class="ysmoney">
+                <div>已售<span>134</span>件</div>
+              </div>
+            </a>
+          </div>
+          <div class="picture">
+            <a class="pic_a">
+              <div class="pic_f">
+                <img src="../../assets/images/mtp/001.jpg">
+              </div>
+              <div class="pictitle">天然苏打水</div>
+              <div class="picmoney">
+                <div class="pm_two">
 					<span>
 						<em>￥</em>
 						9.9
 					</span>
-									<del>
-										￥
-										19.9
-									</del>
-								</div>
-							</div>
-							<div class="ysmoney">
-								<div>已售<span>108</span>件</div>
-							</div>
-						</a>
-					</div>
-					<div class="picture">
-						<a class="pic_a">
-							<div class="pic_f">
-								<img src="../../assets/images/mtp/001.jpg">
-							</div>
-							<div class="pictitle">德雕灰色开关插座面板【暗装】</div>
-							<div class="picmoney">
-								<div class="pm_two">
+                  <del>
+                    ￥
+                    19.9
+                  </del>
+                </div>
+              </div>
+              <div class="ysmoney">
+                <div>已售<span>108</span>件</div>
+              </div>
+            </a>
+          </div>
+          <div class="picture">
+            <a class="pic_a">
+              <div class="pic_f">
+                <img src="../../assets/images/mtp/001.jpg">
+              </div>
+              <div class="pictitle">德雕灰色开关插座面板【暗装】</div>
+              <div class="picmoney">
+                <div class="pm_two">
 					<span>
 						<em>￥</em>
 						3.9
 					</span>
-									<del>
-										￥
-										11.9
-									</del>
-								</div>
-							</div>
-							<div class="ysmoney">
-								<div>已售<span>103</span>件</div>
-							</div>
-						</a>
-					</div>
+                  <del>
+                    ￥
+                    11.9
+                  </del>
+                </div>
+              </div>
+              <div class="ysmoney">
+                <div>已售<span>103</span>件</div>
+              </div>
+            </a>
+          </div>
 
-				</div>
+        </div>
 
-				<div id="photo_two">
-					<img src="../../assets/images/mtp/07.png">
-				</div>
+        <div id="photo_two">
+          <img src="../../assets/images/mtp/07.png">
+        </div>
 
-				<div id="chaozhi">
-					<ul>
-						<li class="first">
-							<a href="#" class="jxone">精选</a>
-						</li>
-						<li>
-							<a href="#">美食</a>
-						</li>
-						<li>
-							<a href="#">百货</a>
-						</li>
-						<li>
-							<a href="#">个护</a>
-						</li>
-						<li>
-							<a href="#">预告</a>
-						</li>
-					</ul>
-				</div>
-				<div id="sphoto">
-					<ul>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">手机防水袋游泳通用触屏密封</h4>
-								<p class="picture_t2">防水轻便</p>
-							</a>
-							<div class="picture_bottom">
+        <div id="chaozhi">
+          <ul>
+            <li class="first">
+              <a href="#" class="jxone">精选</a>
+            </li>
+            <li>
+              <a href="#">美食</a>
+            </li>
+            <li>
+              <a href="#">百货</a>
+            </li>
+            <li>
+              <a href="#">个护</a>
+            </li>
+            <li>
+              <a href="#">预告</a>
+            </li>
+          </ul>
+        </div>
+        <div id="sphoto">
+          <ul>
+            <li
+                v-for="item in productList"
+                :key="item.productId"
+            >
+              <router-link :to="'/productDetail?id=' + item.productId" class="pa">
+                <img :src="item.image">
+                <h4 class="picture_title">{{ item.productName }}</h4>
+                <p class="picture_t2">防水轻便</p>
+              </router-link>
+              <div class="picture_bottom">
 					<span class="ptxt">
 						<span class="ptxt2">
 							<i class="txti">
 								<em>￥</em>
-								8.05
+								{{ item.salesPrice }}
 							</i>
-							￥<del>11.5</del>
+							￥<del>{{ item.price }}</del>
 						</span>
 						<span class="txtd">1年历史最低价</span>
 					</span>
-								<span class="ytxt">
+                <span class="ytxt">
 							<i class="ytxt_ys">已售<span>111</span>件</i>
 					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
+                <a href="#" class="qgou">立即抢购</a>
+              </div>
 
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">板栗南瓜粉糯香甜5斤装</h4>
-								<p class="picture_t2">肉质细腻味如板栗</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								13.9
-							</i>
-							￥<del>19.9</del>
-						</span>
-						<span class="txtd">21天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">已售<span>130</span>件</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
+            </li>
 
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">采小海 无添加盐淡干虾皮 100g</h4>
-								<p class="picture_t2">特级无盐虾皮</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								9.8
-							</i>
-							￥<del>29.9</del>
-						</span>
-						<span class="txtd">1年历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">已售<span>216</span>件</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">夏季男士短裤</h4>
-								<p class="picture_t2">舒适宽松</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								29.9
-							</i>
-							￥<del>59.9</del>
-						</span>
-						<span class="txtd">60天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">已售<span>200</span>件</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">欧诗漫美白套装四件套</h4>
-								<p class="picture_t2">美白补水大牌秒杀</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								99
-							</i>
-							￥<del>159</del>
-						</span>
-						<span class="txtd">107天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">免打孔浴室卫生间置物架挂壁</h4>
-								<p class="picture_t2">双层 免打孔</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								39
-							</i>
-							￥<del>69</del>
-						</span>
-						<span class="txtd">91天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">高清高倍拍照设备</h4>
-								<p class="picture_t2">可连接手机</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								99
-							</i>
-							￥<del>199</del>
-						</span>
-						<span class="txtd">1年历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">夏季性感女香12ml多款可选</h4>
-								<p class="picture_t2">亏本秒杀</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								9.9
-							</i>
-							￥<del>19.9</del>
-						</span>
-						<span class="txtd">191天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">原木抽纸3层24包</h4>
-								<p class="picture_t2">限时抢</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								29.9
-							</i>
-							￥<del>49</del>
-						</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">味滋源每日坚果20gX10包</h4>
-								<p class="picture_t2">8种果仁组合</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								16
-							</i>
-							￥<del>26.9</del>
-						</span>
-						<span class="txtd">1年历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">已售<span>201</span>件</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">特价儿童套装</h4>
-								<p class="picture_t2">两件45三件65</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								24.68
-							</i>
-							￥<del>39</del>
-						</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">12v洗车机便携式刷车神器</h4>
-								<p class="picture_t2">纯铜机芯三年质保</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								96
-							</i>
-							￥<del>252</del>
-						</span>
-						<span class="txtd">1年历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">不锈钢水龙头</h4>
-								<p class="picture_t2">耐用质保</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								8.05
-							</i>
-							￥<del>11.5</del>
-						</span>
-						<span class="txtd">1年历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">高山百香果精选大果24个</h4>
-								<p class="picture_t2">酸酸甜甜</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								29.9
-							</i>
-							￥<del>55</del>
-						</span>
-						<span class="txtd">154天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">热卖中</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/001.jpg">
-								<h4 class="picture_title">实木衣柜三层</h4>
-								<p class="picture_t2">家用实木</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								9.8
-							</i>
-							￥<del>29.9</del>
-						</span>
-						<span class="txtd">1年历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">已售<span>216</span>件</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-						<li>
-							<a href="#" class="pa">
-								<img src="../../assets/images/mtp/002.jpg">
-								<h4 class="picture_title">男士运动长裤</h4>
-								<p class="picture_t2">舒适宽松两件60</p>
-							</a>
-							<div class="picture_bottom">
-					<span class="ptxt">
-						<span class="ptxt2">
-							<i class="txti">
-								<em>￥</em>
-								35
-							</i>
-							￥<del>59</del>
-						</span>
-						<span class="txtd">60天历史最低价</span>
-					</span>
-								<span class="ytxt">
-							<i class="ytxt_ys">已售<span>200</span>件</i>
-					</span>
-								<a href="#" class="qgou">立即抢购</a>
-							</div>
-
-						</li>
-
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TeJia"
+import {getMiaoShaList} from "@/api/miaosha";
+
+export default {
+  name: "TeJia",
+  data() {
+    return {
+      productList: []
     }
+  },
+  created() {
+    getMiaoShaList().then(res => {
+      this.productList = res.rows;
+    })
+  }
+}
 </script>
 
 <style src="../../assets/style/tejia_index.css" scoped></style>
